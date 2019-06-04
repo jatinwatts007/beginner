685477b213ebaa25963bf7c7953353267377b1dc
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2019-06-04 11:02:59 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,avg;
+	clrscr();
+	printf("a=");
+	scanf("%d",&a);
+	printf("b=");
+	scanf("%d",&b);
+	printf("c=");
+	scanf("%d",&c);
+	avg=(a+b+c)/3;
+	printf("average=%d",avg);
+	getch ();
+}
